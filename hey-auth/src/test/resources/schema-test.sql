98e0bc1ce1e4e84d04f9e3fc79fa15ec76bbfdcb
ntan1902 <trinh.an.1902@gmail.com>
2021-07-22 20:38:34 +0700

    Add unit test for SystemController

--- hey-auth/src/test/resources/schema-test.sql
+++ hey-auth/src/test/resources/schema-test.sql
@@ -20,3 +20,8 @@ create table systems
   primary key(id),
   unique index uq_system_name (system_name ASC)
 );
+
+insert into systems(id, system_name, system_key) values
+(1, 'payment', '$2a$10$atTTVVOQoQMksMstiYp3/u6tQaYRG/6S5IrMJmEkw8Yw70kKI9LW2'),
+(2, 'chat', '$2a$10$atTTVVOQoQMksMstiYp3/u6tQaYRG/6S5IrMJmEkw8Yw70kKI9LW2'),
+(3, 'luckey_money', '$2a$10$atTTVVOQoQMksMstiYp3/u6tQaYRG/6S5IrMJmEkw8Yw70kKI9LW2');
