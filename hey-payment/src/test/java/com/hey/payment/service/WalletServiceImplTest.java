c75b3805359e0a00e187aabd1e88efa79ad91394
trongvn <trongvn@vng.com.vn>
2021-08-26 22:45:04 +0700

    Increase core coverage tests.

--- hey-payment/src/test/java/com/hey/payment/service/WalletServiceImplTest.java
+++ hey-payment/src/test/java/com/hey/payment/service/WalletServiceImplTest.java
@@ -4,10 +4,12 @@ import com.hey.payment.api.AuthApi;
 import com.hey.payment.constant.OwnerWalletRefFrom;
 import com.hey.payment.dto.auth_system.GetSystemsResponse;
 import com.hey.payment.dto.system.SystemDTO;
+import com.hey.payment.dto.user.HasWalletResponse;
 import com.hey.payment.entity.System;
 import com.hey.payment.entity.User;
 import com.hey.payment.entity.Wallet;
 import com.hey.payment.exception_handler.exception.BalanceNotEnoughException;
+import com.hey.payment.exception_handler.exception.HadWalletException;
 import com.hey.payment.exception_handler.exception.HaveNoWalletException;
 import com.hey.payment.exception_handler.exception.MaxBalanceException;
 import com.hey.payment.mapper.WalletMapper;
@@ -17,6 +19,8 @@ import com.hey.payment.utils.UserUtil;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -99,7 +103,7 @@ class WalletServiceImplTest {
     }
 
     @Test
-    void transferMoneyThrowSourceHaveNoWallet(){
+    void transferMoneyThrowSourceHaveNoWallet() {
         when(walletRepository.findAndLockWalletById(1L)).thenReturn(Optional.empty());
 
         // when
@@ -243,5 +247,44 @@ class WalletServiceImplTest {
         verify(walletRepository, times(1)).save(any(Wallet.class));
         verify(walletMapper, times(1)).wallet2WalletDTO(any(Wallet.class));
 
+    }
+
+    @SneakyThrows
+    @Test
+    void createWalletThrowsHadWalletException() {
+        User user = User.builder().id("uuid").build();
+
+        when(userUtil.getCurrentUser()).thenReturn(user);
+
+        when(walletRepository.existsByOwnerIdAndRefFrom(user.getId(), OwnerWalletRefFrom.USERS))
+                .thenReturn(true);
+
+        // when
+
+        // then
+        assertThrows(HadWalletException.class,() -> underTest.createWallet());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"true","false"})
+    public void hasWallet(boolean isNull) {
+        // given
+        User user = new User("abc-123");
+        when(userUtil.getCurrentUser()).thenReturn(user);
+        Wallet wallet = null;
+        if (!isNull) {
+            wallet = Wallet.builder()
+                    .ownerId(user.getId())
+                    .id(1L)
+                    .balance(100_000L)
+                    .refFrom(OwnerWalletRefFrom.USERS)
+                    .build();
+        }
+        when(walletRepository.findByOwnerIdAndRefFrom(user.getId(), OwnerWalletRefFrom.USERS)).thenReturn(Optional.ofNullable(wallet));
+        HasWalletResponse expected = new HasWalletResponse(!isNull);
+        // when
+        HasWalletResponse actual = underTest.hasWallet();
+        // then
+        assertEquals(actual, expected);
     }
 }
