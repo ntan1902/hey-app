e68ed559e539076701b241ffdf99716a396426ab
lygioian <lygioian@gmail.com>
2021-08-28 22:54:23 +0700

    hot fix message panel

--- hey-frontend/src/components/message-panel.js
+++ hey-frontend/src/components/message-panel.js
@@ -56,21 +56,23 @@ class MessagePanel extends React.Component {
       <div className="chat-content">
         <div ref={this.props.refProp}></div>
 
-        {this.props.messageItems
-          .slice(0, this.props.loadSize)
-          .map((item, index) => (
-            <ChatItem
-              key={item.id && item.id != "" ? item.id : index}
-              type={item.type}
-              value={item.message}
-              showavatar={item.showavatar}
-              avatar={item.avatar}
-              date={item.createdDate}
-              userId={item.userId}
-              name={item.name}
-              id={item.id}
-            />
-          ))}
+        {this.props.currentSessionId != null &&
+          this.props.userSelectedKeys != [] &&
+          this.props.messageItems
+            .slice(0, this.props.loadSize)
+            .map((item, index) => (
+              <ChatItem
+                key={item.id && item.id != "" ? item.id : index}
+                type={item.type}
+                value={item.message}
+                showavatar={item.showavatar}
+                avatar={item.avatar}
+                date={item.createdDate}
+                userId={item.userId}
+                name={item.name}
+                id={item.id}
+              />
+            ))}
         <div
           ref={this.divLoadMore}
           id="load_more"
@@ -88,6 +90,8 @@ function mapStateToProps(state) {
     messageItems: state.chatReducer.messageItems,
     isAll: state.chatReducer.isAll,
     loadSize: state.chatReducer.loadSize,
+    currentSessionId: state.chatReducer.currentSessionId,
+    userSelectedKeys: state.chatReducer.userSelectedKeys,
   };
 }
 
